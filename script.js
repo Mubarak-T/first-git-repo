9d3c4e86b58af8e791ca6bd725fee5940e26d6e9
Mubarak-T <mubarakthayyil4@gmail.com>
2024-08-11 18:34:49 +0530

    form created and Even numbers to 10 console in js

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-//   let num  declare a variable
+// //   let num  declare a variable
 
-//   num = 10  assign value to variable
-
-  let num2 = num
-
-  num = 20
-
-  let nums = [1,2,3,4,5]
-  let nums2 = [1,2,3,5,4,6]
-  nums[0] = 10
-
-  console.log(num)
-  console.log(num2);
-  console.log(nums);
-  console.log(nums2);
+// //   num = 10  assign value to variable
+// let nums = [1,2,3,4,5]
+//   let num2 = nums
 
 
+
+  
+//   let nums2 = [1,2,3,5,4,6]
+//   nums[0] = 10
+
+//   console.log(nums)
+//   console.log(num2);
+//   console.log(nums);
+//   console.log(nums2);
 
 
 
@@ -24,126 +22,237 @@
 
 
 
-//    string
+
+// //    string
+// //    number
+// //    boolean
+// //    null
+// //    undefined
+// //    object
+// //    array
+// //    function
+
+// //    string
+//   let s1 = "test"
 //    number
+//   let n1 = 10
 //    boolean
+  // let b1 = true
+  // let b2 = false
 //    null
-//    undefined
+//   let nu = null
+//     undefined
+//   let u1 = undefined
 //    object
-//    array
-//    function
-
-//    string
-  let s1 = "test"
-   number
-  let n1 = 10
-   boolean
-  let b1 = true
-  let b2 = false
-   null
-  let nu = null
-    undefined
-  let u1 = undefined
-   object
-  let student = {name: "S1", age: 17, std: 12}
-  let number = [1,2,3,4,5]
+//   let student = {name: "S1", age: 17, std: 12}
+//   let number = [1,2,3,4,5]
 
 
-  let val = null
-   console.log(val);
-   console.log(student.name);
-   console.log(student.age);
-   console.log(student.std);
+//   let val = null
+//    console.log(val);
+//    console.log(student.name);
+//    console.log(student.age);
+//    console.log(student.std);
 
-   student.name = "S1 updated"
-   console.log(student.name);
-   console.log(student.age);
-   console.log(student.std);
+//    student.name = "S1 updated"
+//    console.log(student.name);
+//    console.log(student.age);
+//    console.log(student.std);
 
-   student["name"] = "S1 is updated again"
-   console.log(student.name);
-   console.log(student.age);
-   console.log(student.std);
-
-
-  function consoleStudentValue(){
-      student.name = "S1 updated"
-      console.log(student.name);
-      console.log(student.age);
-      console.log(student.std);
-
-  }
-
-  consoleStudentValue()
+//    student["name"] = "S1 is updated again"
+//    console.log(student.name);
+//    console.log(student.age);
+//    console.log(student.std);
 
 
-//   oprerators
-//   1.arithametic
-//   2.assignment
-//   3.comparison
-//   4.logical
-//   5.termary
+//   function consoleStudentValue(){
+//       student.name = "S1 updated"
+//       console.log(student.name);
+//       console.log(student.age);
+//       console.log(student.std);
 
-//   arithametic
-  console.log(1+1)
-  console.log(3-1);
-  console.log(8/2);
-  console.log(8*2);
-  console.log(8**6);
-  console.log(9%2);
+//   }
+
+//   consoleStudentValue()
+
+
+// //   oprerators
+// //   1.arithametic
+// //   2.assignment
+// //   3.comparison
+// //   4.logical
+// //   5.termary
+
+// //   arithametic
+//   console.log(1+1)
+//   console.log(3-1);
+//   console.log(8/2);
+//   console.log(8*2);
+//   console.log(8**6);
+//   console.log(9%2);
 
 //   assignment 
-  let num = 5
-  num  %= 2
-  console.log(num);
+//   let num = 5
+//   num  %= 2
+//   console.log(num);
 
-//   comparison 
-  console.log(5 == "5");
-  console.log(5 === "5");
-  console.log(5 != "5");
-  console.log(5 !==  "5");
+// //   comparison 
+//   console.log(5 == "5");
+//   console.log(5 === "5");
+//   console.log(5 != "5");
+//   console.log(5 !==  "5");
 
-  console.log(5 > 5);
-  console.log(4 < 5);
-  console.log(5 >= 5);
-  console.log(5 <= 4);
+//   console.log(5 > 5);
+//   console.log(4 < 5);
+//   console.log(5 >= 5);
+//   console.log(5 <= 4);
 
-//   logical
-//   And &
-//   Or |
-//   Not !
+// //   logical
+// //   And &
+// //   Or |
+// //   Not !
 
 //   1 && 1 = true
 //   1 && 0 = false
 //   0 && 1 = false
 //   0 && 0 = false
 
-  console.log(true && true && false)
-  console.log(true && false)
-  console.log(false && true)
-  console.log(false && false)
+  // console.log(true && true && false)
+  // console.log(true && false)
+  // console.log(false && true)
+  // console.log(false && false)
 
-//   1 || 1 = true
-//   1 || 0 = false
-//   0 || 1 = false
-//   0 || 0 = false
+  // 1 || 1 == true
+  // 1 || 0 == false
+  // 0 || 1 ==false
+  // 0 || 0 == false
 
-  console.log(true || true)
-  console.log(true || false)
-  console.log(false || true || false)
-  console.log(false || false || true)
+  // console.log(1 || 1)
+  // console.log(1 || 0)
+  // console.log(0 || 0)
+  // console.log(0 || 1)
 
-//   !true = false
-//   !false = true
+  // !true == false
+  // !false == true
 
-  console.log(!true);
-  console.log(!false);
+  // console.log(!true);
+  // console.log(!false);
 
-// termary
+// // termary
 
-let studentPass = true
+// let studentPass = true
 
-studentPass?console.log("Pass"):console.log("Fail")
+// studentPass?console.log("Pass"):console.log("Fail")
 
-let studentpass = false
-studentpass?console.log("Pass"):console.log("Fail")
+// let studentpass = false
+// studentpass?console.log("Pass"):console.log("Fail")
+
+// let studentPass = true
+
+// studentPass?console.log("Pass"):console.log("Fail")
+
+// let studentpass = false
+
+// studentpass?console.log("Pass"):console.log("Fail")
+
+// let age = 18
+ 
+// if(age >= 18){
+//     console.log("Eligible")
+// }
+// else{
+//     console.log("Not Eligible")
+// }
+
+// let vehicle = 2
+// if (vehicle < 2) {
+//     console.log("Not a Vehicle")
+// }
+// else if (vehicle == 2) {
+//     console.log("bike");
+// }
+// else if(vehicle == 3){
+//     console.log("Auto");
+// }
+// else if(vehicle == 4){
+//     console.log("Car");
+// }
+// else{
+//     console.log("heavy");
+// }
+// loop
+// for loop
+// while loop
+
+
+// for loop
+// for(let i = 0; i <= 5; i++){
+//     console.log(i)
+// }
+
+
+// console.log(nums[0]);
+// console.log(nums[1]);
+// console.log(nums[2]);
+// console.log(nums[3]);
+// console.log(nums[4]);
+// console.log(nums[5]);
+// console.log(nums[6]);
+
+
+// while loop
+// while (j < nums.length) {
+  //   console.log(nums[j]); 
+  //   if (j > 3){
+    //     break
+    //   }
+    //     j++;
+    
+    //   }
+    
+    // let i = 0
+    // j = 0
+//     let nums = [3,5,2,1,6,8,3]
+//     let nums2 = [100,200,300]
+
+
+
+
+//     function valInNums(arr){
+//       for (let i = 0; i <arr.length ; i++){
+//         console.log(arr[i]);
+        
+//       } 
+//     }
+
+// console.log("test");
+// valInNums(nums)
+// valInNums(nums2)
+
+// function valInNums(age){
+//   if (age >= 18) {
+//     console.log("Eligible"); 
+//   }
+//   else{
+//     console.log("Not Eligible");
+//   }
+// }
+
+// valInNums(10)
+// valInNums(18)
+// valInNums(12)
+// valInNums(35)
+
+// if (i % 2 == 0) {
+//   console.log(i);
+// }
+// else{
+//   console.log("Numberb is ODD");
+  
+// }
+
+for (let i =0; i <= 10; i++){
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
